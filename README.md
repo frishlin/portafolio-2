32014c8b8ec491f5636cd70aaacaca5687edb1c5
frishalin@gmail.com <marisolgirls33@gmail.com>
2024-03-05 08:42:25 -0600

    portafolio actualizado

---
+++ README.md
@@ -0,0 +1,7 @@
+[Portafolio de Frishlin](https://frishlin.github.io/)
+
+# Herramientas utilizadas:
+- HTML5
+- CSS3
+- jQuery v3.3.1
+- Bootstrap v4.0.0
---
+++ js/app.js
@@ -0,0 +1,13 @@
+console.log("algo");
+
+$(function(){
+    $('.galeria .contenedor-imagen').on('click', function(){
+        $('#modal').modal;
+        var ruta_imagen = ($(this).find('img').attr('src'));
+        $('#imagen-modal').attr('src', ruta_imagen);
+    });
+
+    $('#modal').on('click', function(){
+        $('#modal').modal('hide');
+    });
+})
